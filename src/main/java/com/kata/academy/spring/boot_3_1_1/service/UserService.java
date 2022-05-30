f80bbda449d0a422f09da492e0a96e29f8d2c833
RalfMog <104436602+RalfMog@users.noreply.github.com>
2022-05-30 13:13:59 +0300

    Update UserService.java

--- src/main/java/com/kata/academy/spring/boot_3_1_1/service/UserService.java
+++ src/main/java/com/kata/academy/spring/boot_3_1_1/service/UserService.java
@@ -7,7 +7,10 @@ import java.util.List;
 public interface UserService {
 
     public User findById(Long id);
+
     public List<User> findAll();
+
     public User saveUser(User user);
+
     public void deleteById(Long id);
 }
